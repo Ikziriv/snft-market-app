e9f07e0627735474c0a994023d9b59da7a11fc86
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-03-13 15:32:39 +0700

    Latest update 13/03/2022 3:32PM

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { plugin as md, Mode } from 'vite-plugin-markdown'
-import hljs from 'highlight.js'
-import hljs_svelte from 'highlightjs-svelte'
+import { plugin as md, Mode } from 'vite-plugin-markdown';
+import hljs from 'highlight.js';
+import hljs_svelte from 'highlightjs-svelte';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(
+    fileURLToPath(
+        import.meta.url)
+); // jshint ignore:line
 hljs_svelte(hljs)
 const mdPlugin = md({
         mode: Mode.HTML,
@@ -26,18 +32,42 @@ const mdPlugin = md({
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: preprocess(),
+    preprocess: [
+        preprocess({
+            postcss: true,
+            preserve: ['ld+json', 'module'],
+            typescript: true,
+        })
+    ],
 
     kit: {
         adapter: adapter(),
-
+        ssr: true,
+        prerender: {
+            crawl: true,
+            enabled: true,
+            onError: 'fail',
+            pages: ['*'],
+        },
         methodOverride: {
             allowed: ['PUT', 'PATCH', 'DELETE'],
         },
 
-        vite: {
+        vite: () => ({
+            resolve: {
+                alias: {
+                    $stores: resolve(__dirname, './src/libs/stores'),
+                    $screens: resolve(__dirname, './src/libs/screens'),
+                    $components: resolve(__dirname, './src/libs/components'),
+                    $page: resolve(__dirname, './src/libs/page'),
+                    $shared: resolve(__dirname, './src/libs/components/shared'),
+                    $models: resolve(__dirname, './src/libs/models'),
+                    $data: resolve(__dirname, './src/libs/data'),
+                    $utils: resolve(__dirname, './src/libs/utils'),
+                },
+            },
             plugins: [mdPlugin],
-        },
+        }),
     }
 };
 
